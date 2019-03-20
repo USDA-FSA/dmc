3f1d6b2f51f53232e4fc959b347855c803e896f8
Casey Hill <bignamehere@gmail.com>
2019-03-20 16:42:14 -0500

    adding prototype kit files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////////////////
+'use strict';
+
+//
+import jQuery from 'jquery';
+
+
+// Project-specific styles
+
+
+// Project-specific JS
+//import './js/docs';
+//import './js/prototype';
